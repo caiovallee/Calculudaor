711dc96f384a78343fa6c07c28e34e5fceb3d2bf
caiovallee <caiohgvalle.14@gmail.com>
2023-05-10 12:59:29 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Calculudaor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtResultado = new TextBox();
+            labelOperecao = new Label();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            button4 = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn6 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            button14 = new Button();
+            button16 = new Button();
+            button20 = new Button();
+            SuspendLayout();
+            // 
+            // txtResultado
+            // 
+            txtResultado.Location = new Point(26, 62);
+            txtResultado.Name = "txtResultado";
+            txtResultado.Size = new Size(336, 23);
+            txtResultado.TabIndex = 0;
+            txtResultado.TextAlign = HorizontalAlignment.Right;
+            txtResultado.TextChanged += txtResultado_TextChanged;
+            // 
+            // labelOperecao
+            // 
+            labelOperecao.AutoSize = true;
+            labelOperecao.BackColor = SystemColors.ButtonShadow;
+            labelOperecao.Location = new Point(47, 15);
+            labelOperecao.Name = "labelOperecao";
+            labelOperecao.Size = new Size(0, 15);
+            labelOperecao.TabIndex = 1;
+            // 
+            // btn7
+            // 
+            btn7.Location = new Point(4, 101);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(66, 53);
+            btn7.TabIndex = 2;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
+            // 
+            // btn8
+            // 
+            btn8.Location = new Point(85, 101);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(66, 53);
+            btn8.TabIndex = 3;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
+            // 
+            // btn9
+            // 
+            btn9.Location = new Point(166, 101);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(66, 53);
+            btn9.TabIndex = 4;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(247, 101);
+            button4.Name = "button4";
+            button4.Size = new Size(66, 53);
+            button4.TabIndex = 5;
+            button4.Text = "+";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // btn4
+            // 
+            btn4.Location = new Point(4, 171);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(66, 53);
+            btn4.TabIndex = 7;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
+            // 
+            // btn5
+            // 
+            btn5.Location = new Point(85, 171);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(66, 53);
+            btn5.TabIndex = 8;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
+            // 
+            // btn6
+            // 
+            btn6.Location = new Point(166, 171);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(66, 53);
+            btn6.TabIndex = 9;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(247, 171);
+            button9.Name = "button9";
+            button9.Size = new Size(66, 53);
+            button9.TabIndex = 10;
+            button9.Text = "-";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(328, 101);
+            button10.Name = "button10";
+            button10.Size = new Size(66, 261);
+            button10.TabIndex = 11;
+            button10.Text = "=";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(4, 244);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(66, 53);
+            btn1.TabIndex = 12;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += button11_Click;
+            // 
+            // btn2
+            // 
+            btn2.Location = new Point(85, 244);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(66, 53);
+            btn2.TabIndex = 13;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // btn3
+            // 
+            btn3.Location = new Point(166, 244);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(66, 53);
+            btn3.TabIndex = 14;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(247, 244);
+            button14.Name = "button14";
+            button14.Size = new Size(66, 53);
+            button14.TabIndex = 15;
+            button14.Text = "x";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(4, 303);
+            button16.Name = "button16";
+            button16.Size = new Size(228, 59);
+            button16.TabIndex = 17;
+            button16.Text = "0";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += button16_Click;
+            // 
+            // button20
+            // 
+            button20.Location = new Point(247, 308);
+            button20.Name = "button20";
+            button20.Size = new Size(66, 49);
+            button20.TabIndex = 21;
+            button20.Text = "/";
+            button20.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(406, 370);
+            Controls.Add(button20);
+            Controls.Add(button16);
+            Controls.Add(button14);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(button4);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(labelOperecao);
+            Controls.Add(txtResultado);
+            ForeColor = SystemColors.ControlDarkDark;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox txtResultado;
+        private Label labelSoma;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Button button4;
+        private Button button5;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button button9;
+        private Button button10;
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private Button button20;
+        private Label labelOperecao;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+namespace Calculudaor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtResultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "9";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
